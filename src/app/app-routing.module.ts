3d88d27c71ccd83255cf421752f495af05b92123
carlos <carlosolivares496@gmail.com>
2023-03-27 11:22:59 -0300

    2 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "AppComponent", component:AppComponent, pathMatch: "full"},
+  {path :"", component:LoginComponent, pathMatch: "full"},
+  {path :"home", component:HomeComponent, pathMatch: "full"}
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { routing } from "./app-routing.module"; 
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+declarations: [AppComponent, LoginComponent, HomeComponent],
+imports: [BrowserModule, routing, FormsModule, HttpClientModule],
+providers: [],
+bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {}
+
+
+
